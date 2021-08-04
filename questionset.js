09aded935adf75ce0d535b5a190bdb980b9ce36d
suraj <spcoderme@gmail.com>
2021-08-05 03:19:40 +0530

    first creation by suraj pawar

---
+++ questionset.js
@@ -0,0 +1,82 @@
+
+const myQues = [
+    {
+        question: " Who invented the Light Bulb?",
+        answers: { a: "Thomas Alva Edison", b: "arya bhatt", c: "Alexander Fleming", d: "Alexander Graham Bell" },
+        trueAns: 'a'
+    },
+    {
+        question: "  Which planet in our solar system is known as the Red Planet?",
+        answers: { a: "Jupiter", b: "Mars", c: "Venus", d: "Mercury" },
+        trueAns: 'b'
+    },
+    {
+        question: "What is the name of the biggest planet in our solar system?",
+        answers: { a: "Jupiter", b: "Mars", c: "Venus", d: "Mercury"},
+        trueAns: 'a'
+    },
+    {
+        question: " Who invented the Telephone?",
+        answers: { a: "Thomas Alva Edison", b: "Alexander Graham Bell", c: "Alexander Fleming", d: "The Wright Brothers" },
+        trueAns: 'b'
+    },
+    {
+        question: " Who discovered Penicillin?",
+        answers: { a: "The Wright Brothers", b: "arya bhatt", c: "Alexander Fleming", d: "Johannes Gutenberg" },
+        trueAns: 'c'
+    },
+    {
+        question: " Which is the hottest planet in our solar system?",
+        answers: {a: "Jupiter", b: "Mars", c: "Venus", d: "Mercury" },
+        trueAns: 'c'
+    },
+{
+        question: " Which planet in our solar system is known as the Blue Planet?",
+        answers: {a: "Jupiter", b: "Mars", c: "Venus", d: "Earth" },
+        trueAns: 'd'
+    },
+    {
+        question: " Who invented the Printing Press?",
+        answers: { a: "Johannes Gutenberg", b: "arya bhatt", c: "Alexander Fleming", d: "The Wright Brothers" },
+        trueAns: 'a'
+    },
+    {
+        question: " What is the name of the largest moon of Saturn?",
+        answers: { a: "Neptune", b: "Titan", c: "Mars", d: "Earth" },
+        trueAns: 'b'
+    },
+    {
+        question: " Which planet in our Solar System takes the longest time to complete its revolution around the Sun?",
+        answers: { a: "Neptune", b: "Titan", c: "Venus", d: "Saturn" },
+        trueAns: 'a'
+    },
+    {
+        question: " Which planet in our Solar System has thousands of spectacular rings around it?",
+        answers: {  a: "Neptune", b: "Titan", c: "Saturn", d: "Mars" },
+        trueAns: 'c'
+    },
+    {
+        question: " French scientist Louis Pasteur famous for?",
+        answers: { a: "Maleria", b: "vaccination", c: "Antiseptic", d: "AIDS" },
+        trueAns: 'b'
+    },
+    {
+        question: " Which is the smallest bird in the world?",
+        answers: { a: "Bee Hummingbirds", b: "Eagle", c: "Pheonix", d: "Kingfisher" },
+        trueAns: 'a'
+    },
+    {
+        question: " Who invented the Computer?",
+        answers: { a: "Elon Musk", b: "Johannes Gutenberg", c: "Charles Babbage", d: "Steave Jobs" },
+        trueAns: 'c'
+    },
+    {
+        question: " Who are the founders of Google?",
+        answers: { a: "Charles Babbage", b: "Steave Jobs", c: "Charles Babbage", d: "Larry Page and Sergey Brin" },
+        trueAns: 'd'
+    }
+];
+myQues;
+
+
+
---
+++ script.js
@@ -0,0 +1,108 @@
+var dashQues;
+dashQues = document.getElementById("quk")
+var dashOpt1;
+dashOpt1 = document.getElementById("optionOne");
+var dashOpt2;
+dashOpt2 = document.getElementById("optionTwo");
+var dashOpt3;
+dashOpt3 = document.getElementById("optionThree");
+var dashOpt4;
+dashOpt4 = document.getElementById("optionFour");
+
+var n = Math.floor(Math.random() * 15);
+
+
+
+
+
+function genrateQues() {
+    var n = Math.floor(Math.random() * 15);
+
+    let opt = myQues[n].answers;
+    // let op2 = myQues[n].answers.b;
+    //let op3 = myQues[n].answers.c;
+    //let op4 = myQues[n].answers.d;
+
+    dashQues.innerHTML = myQues[n].question;
+    dashOpt1.innerHTML = opt.a;
+    dashOpt2.innerHTML = opt.b;
+    dashOpt3.innerHTML = opt.c;
+    dashOpt4.innerHTML = opt.d;
+}
+
+function myFun() {
+
+    return document.querySelector(".quesPaper").style.display = 'block';
+
+}
+function callBoth() {
+    genrateQues();
+    myFun();
+    dashOpt1.style.backgroundColor = "#fff";
+    dashOpt2.style.backgroundColor = "#fff";
+    dashOpt3.style.backgroundColor = "#fff";
+    dashOpt4.style.backgroundColor = "#fff";
+}
+/*var dashQues;
+dashQues = document.getElementById("quk")
+var dashOpt1;
+dashOpt1 = document.getElementById("optionOne");
+var dashOpt2;
+dashOpt2 = document.getElementById("optionTwo");
+var dashOpt3;
+dashOpt3 = document.getElementById("optionThree");
+var dashOpt4;
+dashOpt4 = document.getElementById("optionFour");
+dashOpt1,dashOpt2,dashOpt3,dashOpt4*/
+
+
+function scanAns(id) {
+    let opt = myQues[n];
+    function scan(a,b,c,d) {
+
+
+        if (opt.trueAns == d) {
+            return dashOpt1.style.backgroundColor = "green";
+
+        }
+        else {
+            return dashOpt1.style.backgroundColor = "red";
+        }
+
+    };
+    switch (id) {
+        case id = dashOpt1:
+            dashOpt1.style.backgroundColor = "yellow";
+
+            setTimeout(scan, 3000);
+
+
+            break;
+        case id = dashOpt2:
+            dashOpt2.style.backgroundColor = "yellow";
+
+            setTimeout(scan, 3000);
+
+
+            break;
+        case id = dashOpt3:
+            dashOpt3.style.backgroundColor = "yellow";
+
+            setTimeout(scan, 3000);
+
+
+            break;
+        case id = dashOpt4:
+            dashOpt4.style.backgroundColor = "yellow";
+            setTimeout(scan, 3000);
+
+
+
+            break;
+
+    }
+
+
+
+}
+
